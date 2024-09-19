1b26a613e5dd7a9530e7a11bda4a3bb0742352fd
Max <maxabouchar@gmail.com>
2024-09-18 17:23:09 -0700

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['SFRounded', 'ui-rounded', 'SF Pro Rounded', 'system-ui', 'Helvetica Neue', 'Arial', 'Helvetica', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
